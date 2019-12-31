9fc95e239ff6ae279dc650c3e6c1168df43d437f
Wanyun <wyunyang@gmail.com>
2019-12-31 16:43:28 +0000

    Integrated pusher chatkit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { ChatkitProvider, TokenProvider, withChatkit } from '@pusher/chatkit-client-react';
+import Chat from '../chat/Chat';
+
+const instanceLocator = 'v1:us1:4a1d632e-8719-414e-947c-81b0352f1f52';
+const userId = 'Alice'; // todo: In a real application this would be loaded from your authentication system
+
+const tokenProvider = new TokenProvider({
+  url: 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/4a1d632e-8719-414e-947c-81b0352f1f52/token' // todo: Test token, needs to generate real token somehow in prod
+});
 
 const Home: React.FC = () => {
   return (
@@ -10,17 +19,26 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <ChatkitProvider 
+          instanceLocator={instanceLocator}
+          tokenProvider={tokenProvider}
+          userId={userId}
+        >
+          <Chat otherUserId={'Bob'}/>
+        </ChatkitProvider>
       </IonContent>
     </IonPage>
   );
 };
+
+const WelcomeMessage = withChatkit(props => {
+  return (
+    <div>
+      {props.chatkit.isLoading 
+      ? 'Connecting to Chatkit...'
+      : `Hello ${props.chatkit.currentUser.name}!`}
+    </div>
+  )
+})
 
 export default Home;
